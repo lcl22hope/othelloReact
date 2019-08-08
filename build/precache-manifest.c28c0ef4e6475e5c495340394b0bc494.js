e2475c0a9e981d8ea38c5641f1189d4efb550ec1
lcl22hope <lcl22hope@gmail.com>
2019-08-08 18:45:04 +0800

    add .env

---
+++ build/precache-manifest.c28c0ef4e6475e5c495340394b0bc494.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0878953169bc4ebfd7869361c2f8cee2",
+    "url": "https://lcl22hope.github.io/othelloReact/index.html"
+  },
+  {
+    "revision": "ee781781fc74433a4e77",
+    "url": "https://lcl22hope.github.io/othelloReact/static/css/main.7813ae71.chunk.css"
+  },
+  {
+    "revision": "d0d997294a23536c6168",
+    "url": "https://lcl22hope.github.io/othelloReact/static/js/2.65f0536e.chunk.js"
+  },
+  {
+    "revision": "ee781781fc74433a4e77",
+    "url": "https://lcl22hope.github.io/othelloReact/static/js/main.cc4710c8.chunk.js"
+  },
+  {
+    "revision": "7460d2481e456459a1a4",
+    "url": "https://lcl22hope.github.io/othelloReact/static/js/runtime~main.45cb6494.js"
+  }
+]);
